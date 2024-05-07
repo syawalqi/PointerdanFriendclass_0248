af20a6273b453e5e795fc4fa173cd1f6615cfa16
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-07 16:01:32 +0700

    membuat class pelajar

--- FriendFunctionDua.cpp
+++ FriendFunctionDua.cpp
@@ -7,5 +7,14 @@ class manusia
 {
     public:
      void showNilaiPelajar(pelajar &x);
-}
+};
 
+class pelajar
+{
+    private:
+    int nilai;
+
+    public:
+    pelajar() {nilai = 100;}
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
