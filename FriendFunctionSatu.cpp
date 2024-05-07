5a573d3c51560b2640c8a7f663ace6aba447b537
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-07 15:54:32 +0700

    membuat void setNama

--- FriendFunctionSatu.cpp
+++ FriendFunctionSatu.cpp
@@ -7,7 +7,12 @@ class mahasiswa
     string nama;
     public :
     friend void setNama(mahasiswa &a, string);
-
 };
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
 
 
