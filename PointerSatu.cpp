5528c4109857d8e56115378f927485e249482617
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-07 15:16:54 +0700

    membuat int main mahasiswa

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -8,3 +8,12 @@ class mahasiswa{
         cout << " No Induk =" << nim << endl;
     }
 };
+
+int main(){
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // member acces operator
+
+    mahasiswa &refMhs = mhs; // pointer referense refMhs
+    refMhs.nim = 2; // member acces operator
+    mhs.showNim();
+}
