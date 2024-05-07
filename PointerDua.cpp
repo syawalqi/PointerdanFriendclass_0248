f76451761bd26d58bb5635f46fe5f81b7c175bd6
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-07 15:26:46 +0700

    membuat int main

--- PointerDua.cpp
+++ PointerDua.cpp
@@ -12,4 +12,11 @@ class mahasiswa
     }
 };
 
-int main
+int main ()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // pointer object mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
