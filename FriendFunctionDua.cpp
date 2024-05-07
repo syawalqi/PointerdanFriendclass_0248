9a5f7036e814fca39486dd4f9e0e157ebba8682c
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-07 16:08:38 +0700

    membuat int main

--- FriendFunctionDua.cpp
+++ FriendFunctionDua.cpp
@@ -19,8 +19,16 @@ public:
     friend void manusia::showNilaiPelajar(pelajar &x);
 };
 
-void manusia::showNilaiPelajar(pelajar &x);
+void manusia::showNilaiPelajar(pelajar &x)
 {
-    cout << x.nilai
+    cout << x.nilai;
+}
+
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
 }
 
